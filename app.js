6352612b9b1d1571141f67e98601596c9e65a6fd
Phillee58 <53276449+Phillee58@users.noreply.github.com>
2020-01-30 09:02:49 -0800

    Updates

--- app.js
+++ app.js
@@ -1,48 +1,101 @@
-// Connect to the data using d3
-  d3.json("./samples.json").then(function(data){
-
-// Use d3 to select the panel with id of `#sample-metadata`
-  var metadata = d3.select("#sample-metadata");
-  metadata.html(""); //clears table
-  console.log(metadata);
-
-  // // Use `Object.entries` to add each key and value pair to the panel
-  Object.entries(data).forEach(([key, value]) => {
-    var row = metadata.append("p");
-    row.text(`${key}: ${value}`);
-    console.log(data);
-  })
-});
-
+function buildMetaData(sample){
+// Function that builds the metadata panel usind d3
+d3.json("./samples.json").then(function(data){
+  // Use d3 to select the panel with id of `#sample-metadata`
+      var sample_metadata = d3.select("#sample-metadata");
+      var metadata = data.metadata;
+      var result = metadata.filter(sampleobj => sampleobj.id==sample)[0]
   
-// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
-    // Use `sample_values` as the values for the bar chart
-    // Use `otu_ids` as the labels for the bar chart
-    // Use `otu_labels` as the hovertext for the chart
+  // Use `.html("") to clear any existing metadata
+      sample_metadata.html("");
+      
+  // Use `Object.entries` to add each key and value pair to the panel
+      Object.entries(result).forEach(([key, value]) => {
+
+        var row = sample_metadata.append("p");
+        row.text(`${key}: ${value}`);
+        console.log(result);
+      })
+    });
+}
+function buildCharts(sample) {
+  
+  // Fetch the sample data for the plots
+  d3.json("./samples.json").then(function(data){
+    var samples = data.samples;
+    var result = samples.filter(sampleobj => sampleobj.id==sample)[0]
+
+  // Build a Bubble Chart using the sample data
+      var xValues = result.otu_ids;
+      var yValues = result.sample_values;
+      var mSize = result.sample_values;
+      var mClrs = result.otu_ids;
+      var tValues = result.otu_labels;
+      
+      var trace_bubble = {
+        x: xValues,
+        y: yValues,
+        text: tValues,
+        mode: 'markers',
+        marker: {
+          size: mSize,
+          color: mClrs
+        }
+      };
+  
+      var data = [trace_bubble];
+      var layout = {
+        xaxis: {title: "OTU ID"}
+      };
+  
+      Plotly.newPlot('bubble', data, layout)
+
+      var pieValue = result.sample_values.slice(0, 10);
+        var pielabel = result.otu_ids.slice(0, 10);
+        var pieHover = result.otu_labels.slice(0, 10);
+  
+        var data = [{
+          values: pieValue,
+          labels: pielabel,
+          hovertext: pieHover,
+          type: 'pie'
+        }];
+  
+        Plotly.newPlot('pie', data);
+    });
+};
 
 
+function init(){
+  d3.json("./samples.json").then(function(data){
+    var selector = d3.select("#selDataset");
+    var sampleNames = data.names;
+    sampleNames.forEach((sample)=>{
+      selector.append("option").text(sample).property("value", sample);
+    })
+    buildMetaData(sampleNames[0])
+    buildCharts(sampleNames[0])
 
+  });
 
-// Create a bubble chart that displays each sample
-    // Use `otu_ids` for the x values
-    // Use `sample_values` for the y values
-    // Use `sample_values` for the marker size
-    // Use `otu_ids` for the marker colors
-    // Use `otu_labels` for the text values
+}
 
+init();
+    
+ 
+// Call updatePlotly() when a change takes place to the DOM
+d3.selectAll("#selDataset").on("change", updatePlotly);
 
+// This function is called when a dropdown menu item is selected
+function updatePlotly() {
 
+  // Use D3 to select the dropdown menu
+  var dropdownMenu = d3.select("#selDataset");
 
-// Display the sample metadata, i.e., an individual's demographic information
+  // Assign the value of the dropdown menu option to a variable
+  var dataset = dropdownMenu.property("value");
 
+  buildMetaData(dataset)
+  buildCharts(dataset)
+};
 
-
-
-
-// Display each key-value pair from the metadata JSON object somewhere on the page
-
-
-
-
-
-// Update all of the plots any time that a new sample is selected
