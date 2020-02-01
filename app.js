e328f0a37dff2bc662e8f8137eb5ff9eaff5fcff
Phillee58 <53276449+Phillee58@users.noreply.github.com>
2020-01-31 16:55:04 -0800

    Final Commit

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 // Define the function to build the data from samples.json
 function buildMetaData(sample){
 
-  // Function that builds the metadata panel usind d3
-  d3.json("./samples.json").then(function(data){
+// Function that builds the metadata panel usind d3
+d3.json("./samples.json").then(function(data){
 
-    // Use d3 to select the panel with id of `#sample-metadata` and default first id
-    var sample_metadata = d3.select("#sample-metadata");
-    var metadata = data.metadata;
-    var result = metadata.filter(sampleobj => sampleobj.id==sample)[0]
-    
-    // Use `.html("") to clear any existing metadata
-    sample_metadata.html("");
-        
-    // Use `Object.entries` to add each key and value pair to the panel
-    Object.entries(result).forEach(([key, value]) => {
-      var row = sample_metadata.append("p");
-      row.text(`${key}: ${value}`);
-    })
-  });
+  // Use d3 to select the panel with id of `#sample-metadata` and default first id
+      var sample_metadata = d3.select("#sample-metadata");
+      var metadata = data.metadata;
+      var result = metadata.filter(sampleobj => sampleobj.id==sample)[0]
+  
+  // Use `.html("") to clear any existing metadata
+      sample_metadata.html("");
+      
+  // Use `Object.entries` to add each key and value pair to the panel
+      Object.entries(result).forEach(([key, value]) => {
+
+        var row = sample_metadata.append("p");
+        row.text(`${key}: ${value}`);
+        console.log(result);
+      })
+    });
 }
 
 // Define the function to build the charts
@@ -28,73 +30,70 @@ function buildCharts(sample) {
     var samples = data.samples;
     var result = samples.filter(sampleobj => sampleobj.id==sample)[0]
 
-    // Build the BUBBLE plot
-    var xValues = result.otu_ids;
-    var yValues = result.sample_values;
-    var mSize = result.sample_values;
-    var mClrs = result.otu_ids;
-    var tValues = result.otu_values;
-    
-    var trace_bubble = {
-      x: xValues,
-      y: yValues,
-      text: tValues,
-      mode: 'markers',
-      marker: {
-        size: mSize,
-        color: mClrs
-      }
-    };
+  // Build the BUBBLE plot
+      var xValues = result.otu_ids;
+      var yValues = result.sample_values;
+      var mSize = result.sample_values;
+      var mClrs = result.otu_ids;
+      var tValues = result.otu_values;
+      
+      var trace_bubble = {
+        x: xValues,
+        y: yValues,
+        text: tValues,
+        mode: 'markers',
+        marker: {
+          size: mSize,
+          color: mClrs
+        }
+      };
+  
+      var data = [trace_bubble];
+      var layout = {
+        xaxis: {title: "OTU ID"}
+      };
+  
+      Plotly.newPlot('bubble', data, layout)
 
-    var data = [trace_bubble];
-    var layout = {
-      xaxis: {title: "OTU ID"}
-    };
-    Plotly.newPlot('bubble', data, layout)
+ // Build the BAR plot
+ var barValue = result.sample_values.slice(0,10).reverse();
+ var otu_ids_bar = result.otu_ids.slice(0,10).map((item => "ITU " + item)).reverse(); 
+ var otu_labels_bar = result.otu_labels.slice(0,10).reverse();
 
+ // create trace variable for the plot
+ var trace_bar = {
+   x: barValue,
+   y: otu_ids_bar,
+   text: otu_labels_bar,
+   type: 'bar',
+   orientation: "h",
+ };
 
-    // Build the BAR plot
-    var barValue = result.sample_values.slice(0,10).reverse();
-    var otu_ids_bar = result.otu_ids.slice(0,10).map((item => "ITU " + item)).reverse(); 
-    var otu_labels_bar = result.otu_labels.slice(0,10).reverse();
+ // Create data variable and display
+ var data_bar = [trace_bar];
+ var layout_bar = {
+   title: "Top 10 OTUs",
+   margin: {
+     l: 100,
+   },
+   xaxis: {title: "Quantity"},
+ };  
 
-    // create trace variable for the plot
-    var trace_bar = {
-      x: barValue,
-      y: otu_ids_bar,
-      text: otu_labels_bar,
-      type: 'bar',
-      orientation: "h",
-    };
+ Plotly.newPlot("bar", data_bar, layout_bar)
 
-    console.log(trace_bar)
+// Build the PIE plot
+var pieValue = result.sample_values.slice(0, 10);
+var pieLabel = result.otu_ids.slice(0, 10);
+var pieHover = result.otu_labels.slice(0, 10);
 
-    // Create data variable and display
-    var data_bar = [trace_bar];
-    var layout_bar = {
-      title: "Top 10 OTUs",
-      margin: {
-        l: 100,
-      },
-      xaxis: {title: "Quantity"},
-    };  
+var data = [{
+  values: pieValue,
+  labels: pieLabel,
+  hovertext: pieHover,
+  type: 'pie'
+}];
 
-
-    Plotly.newPlot("bar", data_bar, layout_bar)
-
-    // Build the PIE plot
-    var pieValue = result.sample_values.slice(0, 10);
-    var pieLabel = result.otu_ids.slice(0, 10);
-    var pieHover = result.otu_labels.slice(0, 10);
-
-    var data_pie = [{
-      values: pieValue,
-      labels: pieLabel,
-      hovertext: pieHover,
-      type: 'pie'
-    }];
-
-    Plotly.newPlot('pie', data_pie);
+Plotly.newPlot('pie', data);
   });
 }
 
